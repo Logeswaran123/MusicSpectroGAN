06b6af5a866a718f11647a71769d460807e3a833
Loges <logiflash99037@gmail.com>
2023-04-14 23:22:58 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+
+
+
+
+class MusicSpectroGAN():
+    def __init__(self, ) -> None:
+        pass
+
+    def model(self, ):
+        pass
+
+    def _generator(self, ):
+        pass
+
+    def _discriminator(self, ):
+        pass
